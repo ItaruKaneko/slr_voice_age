700e325cdea97683b21083caaf380f710f79f5d1
ItaruKaneko <itaru-k@acm.org>
2024-02-17 07:22:51 +0900

    all_age_files_f0  two pass processing

--- src/all_age_files.py
+++ src/all_age_files.py
@@ -20,8 +20,8 @@ def process_single_wav_file(folder1,fn1, age1):
 
 def process_all_age_file_list(age_file_table,):
     # age_file_list の全フォルダの全wavファイルを処理するループ
-    maxlen1 = 0
     for spkr_age_record in age_file_table:
+        maxlen1 = 0
         # process the speaker specified by spkr_age_record
         spkr_id = spkr_age_record[0]  # speaker number
         age1 = int(spkr_age_record[1]) # age of the speaker
@@ -36,7 +36,6 @@ def process_all_age_file_list(age_file_table,):
                 maxlen1 = len1
             print(spkr_id, wav_fn, age1, len1, maxlen1)
     print('maxlen1 = ', maxlen1)
-    return(maxlen1)
 
 # 現在のフォルダを表示した後指定したフォルダから情報を読み込む
 print('corrent directory : ' , os.getcwd())
--- src/all_age_files_f0.py
+++ src/all_age_files_f0.py
@@ -20,43 +20,56 @@ def read_tab_separated_file(filename):
             data.append(split_line)
     return data
 
-def process_single_wav_file(folder1,fn1, age1):
+def process_single_wav_file(pass1,folder1,fn1, age1):
     sz1 = os.path.getsize(folder1 + '/' + fn1)
-    print(fn1, age1, sz1)
+    if pass1 == 1:
+        pass
+    elif pass1 == 2:
+        sr, y = wavfile.read(folder1 + '/' + fn1)     # sr sampling rate, y waveform
+        # x = [q/sr for q in np.arange(0, len(y), 1)]
+        # plt.figure(0)
+        # plt.plot(x,y, color="blue")
+        # plt.show()
+        plt.figure(2)
+        y = y.astype(np.float64)
+        _f0, _time = pw.dio(y, sr)
+        f0 = pw.stonemask(y, _f0, _time, sr)
+        plt.plot(f0, linewidth=3, color="green", label="F0 contour")
+        plt.legend(fontsize=10)
+        plt.show()
+    else:
+        raise ValueError("pass1 should be 1 or 2")
+    return(sz1)
 
-    sr, y = wavfile.read(folder1 + '/' + fn1)     # 周波数と振幅値の抽出
-    # x = [q/sr for q in np.arange(0, len(y), 1)]
-    # plt.figure(0)
-    # plt.plot(x,y, color="blue")
-    # plt.show()
-    plt.figure(2)
-    y = y.astype(np.float64)
-    _f0, _time = pw.dio(y, sr)
-    f0 = pw.stonemask(y, _f0, _time, sr)
-    plt.plot(f0, linewidth=3, color="green", label="F0 contour")
-    plt.legend(fontsize=10)
-    plt.show()
+def process_all_age_file_list(age_file_table,):
+    # process pass 1 and pass 2 for all files
+    for pass1 in range(1,3):    # pass1 ==1 and pass1 == 2
+        # process one record in age_file_table
+        for spkr_age_record in age_file_table:
+            maxlen1 = 0
+            # process the speaker specified by spkr_age_record
+            spkr_id = spkr_age_record[0]  # speaker number
+            age1 = int(spkr_age_record[1]) # age of the speaker
+            # construct folder path
+            spkr_folder = wave_file_folder + '/SPEAKER' + spkr_id 
+            # open wave file
+            wav_file_list = os.listdir(spkr_folder)
+            for wav_fn in wav_file_list:
+                # ここで1ファイルの処理の関数を使う
+                len1 = process_single_wav_file(pass1,spkr_folder,wav_fn,age1)
+                if maxlen1 < len1:
+                    maxlen1 = len1
+                print(spkr_id, wav_fn, age1, len1, maxlen1)
+        print('maxlen1 = ', maxlen1)
 
 # 現在のフォルダを表示した後指定したフォルダから情報を読み込む
 print('corrent directory : ' , os.getcwd())
 print('reading ', age_file_list_filename, '...')
-age_file_list = read_tab_separated_file(age_file_list_filename)
+age_file_table = read_tab_separated_file(age_file_list_filename)
 
 # データの確認
-print(len(age_file_list) , ' data had been raead.')
+print(len(age_file_table) , ' data had been raead.')
 
-# age_file_list の全フォルダの全wavファイルを処理するループ
-for spkr_age_record in age_file_list:
-    # ここに各ファイルに対する処理を書く
-    spkr_id = spkr_age_record[0]
-    age1 = int(spkr_age_record[1])
-    print("spkr_id : ", spkr_id)
-    # spkr_id のフォルダ
-    spkr_folder = wave_file_folder + '/SPEAKER' + spkr_id 
-    wav_file_list = os.listdir(spkr_folder)
-    for wav_fn in wav_file_list:
-        # ここで1ファイルの処理の関数を使う
-        process_single_wav_file(spkr_folder,wav_fn,age1)
-
+process_all_age_file_list(age_file_table)
 
 print('done')
