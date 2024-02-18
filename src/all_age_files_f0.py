b3eebe00e84e7b7fbce3820a56ab112c77588cee
ItaruKaneko <itaru-k@acm.org>
2024-02-18 11:34:24 +0900

    all_age_files_f0 - buffer is magaged to be appropreate length for all speakers

--- src/all_age_files_f0.py
+++ src/all_age_files_f0.py
@@ -13,7 +13,7 @@ from scipy.io import wavfile
 # ファイル名を指定
 age_file_list_filename = "../slr101/speechocean762/test/spk2age"
 wave_file_folder = "../slr101/speechocean762/WAVE"
-max_fulllen = 60000000 # size of f0 enverope frequency spectrum
+buf_len = 50000 # size of f0 enverope frequency spectrum
 
 # tab 区切りの表をファイルから読み込み、リストのリストで返す
 def read_tab_separated_file(filename):
@@ -34,9 +34,6 @@ def process_single_wav_file(pass1,folder1,fn1, age1,f0_buf, f0_bp):
     elif pass1 == 2:
         sr, y = wavfile.read(folder1 + '/' + fn1)     # sr sampling rate, y waveform
         # x = [q/sr for q in np.arange(0, len(y), 1)]
-        # plt.figure(0)
-        # plt.plot(x,y, color="blue")
-        # plt.show()
         l = len(y)
         # y1 = np.zeros(len(f0_env_fs),dtype=float)
         # y1[0:l] = y.astype(np.float64)
@@ -47,42 +44,45 @@ def process_single_wav_file(pass1,folder1,fn1, age1,f0_buf, f0_bp):
             raise ValueError("estimated n_frame mismatch")
         sz2 = len(f0)
         f0_buf[f0_bp:f0_bp+sz2] = f0
-        plt.plot(f0_buf[0:2000])
-        # plt.legend(fontsize=10)
-        plt.show()
+        # plt.plot(f0_buf[0:2000])
+        # plt.show()
     else:
         raise ValueError("pass1 should be 1 or 2")
     return(n_frame)
 
-def process_single_speaker(pass1,spkr_age_record,f0_buf,f0_bp):
+def process_single_speaker(pass1,spkr_age_record):
     # process the speaker specified by spkr_age_record
+    f0_buf = np.zeros(buf_len)
     spkr_id = spkr_age_record[0]  # speaker number
     age1 = int(spkr_age_record[1]) # age of the speaker
     # construct folder path
     spkr_folder = wave_file_folder + '/SPEAKER' + spkr_id 
     # open wave file
     wav_file_list = os.listdir(spkr_folder)
+    f0_bp = 0
     for wav_fn in wav_file_list:
         # ここで1ファイルの処理の関数を使う
-        len1 = process_single_wav_file(pass1,spkr_folder,wav_fn,age1,f0_buf, f0_bp)
-        if max_fulllen < f0_bp + len1:
-            raise ValueError("fulllen overflow")
-        print(spkr_id, wav_fn, age1, len1, f0_bp)
+        len1 = process_single_wav_file(pass1,spkr_folder,wav_fn,age1,f0_buf,f0_bp)
+        if buf_len < f0_bp + len1:
+            raise ValueError("buf_len overflow")
+        # print(spkr_id, wav_fn, age1, len1, f0_bp)
         f0_bp += len1
-    return(f0_bp)
+    np.fft.
 
 def process_all_age_file_list(age_file_table,):
     # process pass 1 and pass 2 for all files
     plt.figure(2)
     plt.plot(linewidth=1, color="green", label="F0 contour")
-    full_len = 0
+    # all_max1
+    #  - maximum lenght in all speakers are determined in pass 1 and then kept through pass 2
+    all_max1 = 0
     for pass1 in range(1,3):    # pass1 ==1 and pass1 == 2
-        f0_buf = np.zeros(full_len)
-        f0_bp = 0
+        print('pass1 = ', pass1)
         for spkr_age_record in age_file_table[1:20]:
-            f0_bp = process_single_speaker(pass1,spkr_age_record,f0_buf,f0_bp)
-        full_len = f0_bp
-        print('full_len = ', f0_bp)
+            spkr_max1 = process_single_speaker(pass1,spkr_age_record)
+            if all_max1 < spkr_max1:
+                all_max1 = spkr_max1
+            print('max_len_in_all_spkr', spkr_max1, all_max1)
 
 # 現在のフォルダを表示した後指定したフォルダから情報を読み込む
 print('corrent directory : ' , os.getcwd())
