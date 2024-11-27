5a35b9676e48b65bd9697312c09fa75e164ad538
ItaruKaneko <itaru-k@acm.org>
2024-11-27 22:10:07 +0900

    exp6 ndata=125 age 0-18

--- src/all_age_files_f0_blk.py
+++ src/all_age_files_f0_blk.py
@@ -1,7 +1,7 @@
 # all_age_files_f0_blk.py version
-# exp 5 bug fixed
+# exp 6
 #  ndata= 125
-#  age 18-60
+#  age 0-18
 # Trying to change block sample trial
 # small data for testing -> change n_data to 125 for full set
 # transfer sample to 0.2~0.5 sec chunk
@@ -256,7 +256,7 @@ def interpolate1(pre_interf0):
 # /SPEAKERnn contains multiple wave files
 def process_single_speaker(pass1,age1, blk_X, blk_y, bx, spkr_id1):
     # process the speaker specified by spkr_age_record
-    if age1 < 19:
+    if age1 > 18:
         return(bx) # do nothing
     bx1 = bx
     f0= np.zeros(buf_len, dtype = float)
